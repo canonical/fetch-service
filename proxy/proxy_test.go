1e825aea08369b5259087df695faae00af13314b
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2023-06-11 17:45:12 -0300

    service,proxy: bootstrap basic proxy server
    Implement basic pass-all HTTP proxy.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ proxy/proxy_test.go
@@ -0,0 +1,88 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright 2023 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package proxy_test
+
+import (
+	"crypto/tls"
+	"io"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/canonical/fetch-service/proxy"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type proxySuite struct{}
+
+var _ = Suite(&proxySuite{})
+
+// Test file transfer using the proxy.
+func (s *proxySuite) TestProxyDownload(c *C) {
+	// start the fetch service proxy
+	ch := make(chan interface{}, 1)
+	p := proxy.NewHttpProxy(5566, ch)
+
+	err := p.Start()
+	c.Assert(err, IsNil)
+	defer p.Stop()
+
+	time.Sleep(1 * time.Second)
+
+	// download a test file
+	proxyURL, err := url.Parse("http://localhost:5566")
+	c.Assert(err, IsNil)
+
+	url, err := url.Parse("https://launchpadlibrarian.net:443/592566337/hello_2.10-2ubuntu4_amd64.deb")
+	c.Assert(err, IsNil)
+
+	transport := &http.Transport{
+		Proxy:           http.ProxyURL(proxyURL),
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   30 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	c.Assert(err, IsNil)
+
+	resp, err := client.Do(req)
+	c.Assert(err, IsNil)
+	c.Assert(resp.StatusCode, Equals, 200)
+
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	c.Assert(err, IsNil)
+
+	// check downloaded artifact information
+	msg := <-ch
+	info := msg.(proxy.DownloadInfo)
+
+	c.Assert(info.StatusCode, Equals, 200)
+	c.Assert(info.Method, Equals, "GET")
+	c.Assert(info.ContentType, Equals, "application/x-debian-package")
+}
---
+++ service/service_test.go
@@ -0,0 +1,52 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright 2023 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package service_test
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/canonical/fetch-service/proxy"
+	"github.com/canonical/fetch-service/service"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type serviceSuite struct {
+	port int
+}
+
+var _ = Suite(&serviceSuite{})
+
+// Check if the proxy is created with the correct port number.
+func (s *serviceSuite) TestProxyPort(c *C) {
+	restorer := service.MockNewHttpProxy(func(port int, ch chan interface{}) *proxy.HttpProxy {
+		s.port = port
+		return &proxy.HttpProxy{}
+	})
+	defer restorer()
+
+	opt := service.Options{Port: 1337}
+
+	svc := service.New(&opt)
+	c.Assert(svc, FitsTypeOf, &service.Service{})
+	c.Assert(s.port, Equals, 1337)
+}
