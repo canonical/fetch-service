eb86794074c1b0d4fa6e401ec9332f571b264079
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2023-07-14 00:03:08 -0300

    metadata: add debian package inspector
    Basic metadata extractor for .deb packages. Better heuristics to
    determine author/author email and license is needed.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ metadata/deb.go
@@ -0,0 +1,240 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright 2023 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package metadata
+
+import (
+	"archive/tar"
+	"bufio"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+	"unicode"
+
+	"github.com/blakesmith/ar"
+	"github.com/klauspost/compress/zstd"
+)
+
+type debInspector struct{}
+
+func (debInspector) Inspect(filename string, md *Metadata, di *DownloadInfo, ctx *InspectionContext) (stop bool, err error) {
+	if md.Type != "application/vnd.debian.binary-package" {
+		return
+	}
+	stop = true
+
+	err = readDebMetadata(filename, md)
+	if err != nil {
+		return
+	}
+
+	pkgsDigest, e, ok := ctx.GetPackagesEntry(md.Sha256)
+	if ok {
+		if md.Name != e.Package || md.Version != e.Version || md.Architecture != e.Architecture || md.Size != e.Size {
+			data := AnnotationDetails{"packages-data": e}
+			md.Annotate(IntegrityViolation, "file.integrity.check", ResultFail).SetDetails(data)
+			return
+		}
+		md.Annotate(Notice, "file.integrity.asserted-by", pkgsDigest)
+	} else {
+		md.Annotate(PolicyViolation, "file.integrity.check", ResultFail)
+	}
+
+	return
+}
+
+// readDebMetadata reads metadata from the deb control file.
+func readDebMetadata(filename string, md *Metadata) error {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	af := ar.NewReader(f)
+
+	for {
+		h, err := af.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		switch h.Name {
+		case "debian-binary":
+			err = parseDebianBinary(af, md)
+			if err != nil {
+				return err
+			}
+		case "control.tar.zst", "control.tar.zstd":
+			zf, err := zstd.NewReader(af, zstd.WithDecoderConcurrency(1))
+			if err != nil {
+				return err
+			}
+			err = parseControlTar(zf, md)
+			if err != nil {
+				return err
+			}
+		case "data.tar.zst", "data.tar.zstd":
+			zf, err := zstd.NewReader(af, zstd.WithDecoderConcurrency(1))
+			if err != nil {
+				return err
+			}
+			err = parseDataTar(zf, md)
+			if err != nil {
+				return err
+			}
+			// TODO: add gzip readers
+		}
+	}
+
+	return nil
+}
+
+func parseDebianBinary(af io.Reader, md *Metadata) error {
+	sc := bufio.NewScanner(af)
+	sc.Split(bufio.ScanLines)
+
+	// Read a single line
+	sc.Scan()
+	line := sc.Text()
+	md.Annotate(Notice, "deb.debian-binary.version", line)
+
+	return nil
+}
+
+func parseControlTar(zf io.Reader, md *Metadata) error {
+	tf := tar.NewReader(zf)
+	for {
+		h, err := tf.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		switch h.Name {
+		case "./control":
+			err = parseControl(tf, md)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func parseControl(tf io.Reader, md *Metadata) error {
+	sc := bufio.NewScanner(tf)
+	sc.Split(bufio.ScanLines)
+
+	for sc.Scan() {
+		line := sc.Text()
+		if line == "" {
+			break
+		}
+
+		// Skip long description
+		if line[0] == ' ' {
+			continue
+		}
+
+		k, v, ok := strings.Cut(line, ":")
+		if !ok {
+			continue
+		}
+		v = strings.TrimSpace(v)
+
+		switch k {
+		case "Package":
+			md.Name = v
+		case "Version":
+			md.Version = v
+		case "Architecture":
+			md.Architecture = v
+		case "Description":
+			runes := []rune(v)
+			runes[0] = unicode.ToUpper(runes[0])
+			md.Description = string(runes)
+		case "Maintainer":
+			md.Vendor = v
+			md.AuthorEmail = v
+		}
+	}
+
+	return nil
+}
+
+func parseDataTar(zf io.Reader, md *Metadata) error {
+	copyright := regexp.MustCompile(`^\./usr/share/doc/[^/]+/copyright$`)
+
+	tf := tar.NewReader(zf)
+	for {
+		h, err := tf.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		switch {
+		case copyright.MatchString(h.Name):
+			err = parseCopyright(tf, md)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func parseCopyright(tf io.Reader, md *Metadata) error {
+	sc := bufio.NewScanner(tf)
+	sc.Split(bufio.ScanLines)
+
+	for sc.Scan() {
+		line := sc.Text()
+
+		// Skip long description
+		if len(line) > 0 && line[0] == ' ' {
+			continue
+		}
+
+		// TODO: parse copyright
+
+		k, v, ok := strings.Cut(line, ": ")
+		if !ok {
+			continue
+		}
+
+		switch k {
+		case "License":
+			md.License = v
+		case "Upstream-Contact", "Upstream author":
+			md.Author = v
+		}
+	}
+
+	return nil
+}
