9689938ed4efaea11f4dde6fef5dc64b77f94b9c
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-02-09 23:07:44 -0300

    cli: add option to set the control api port
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- service/service.go
+++ service/service.go
@@ -44,8 +44,8 @@ var proxyNewHttpProxy = proxy.NewHttpProxy
 
 func New(opt *Options) *Service {
 	ch := make(chan interface{})
-	p := proxyNewHttpProxy(opt.Port, opt.Spool, ch)
-	ctl := control.NewServer(9999, ch)
+	p := proxyNewHttpProxy(opt.ProxyPort, opt.Spool, ch)
+	ctl := control.NewServer(opt.ControlPort, ch)
 
 	return &Service{p: p, ctl: ctl, opt: opt, ch: ch}
 }
