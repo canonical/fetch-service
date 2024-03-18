ee87c4506da273b97796e277dbf483633ef4245f
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-03-18 11:00:52 -0300

    feat(snap): set apt release public key
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- snap/local/bin/fetch-service.sh
+++ snap/local/bin/fetch-service.sh
@@ -18,6 +18,10 @@ profile_port="$(snapctl get profile.port)"
 control_port="$(snapctl get control.port)"
 
 
+keyring="/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+key="F6ECB3762474EDA9D21B7022871920D1991BC93C"
+export FETCH_APT_RELEASE_PUBLIC_KEY=$(gpg --export --armor --no-default-keyring --keyring "$keyring" "$key")
+
 exec "${SNAP}/bin/fetch"\
   "--proxy-port=${proxy_port}"\
   "--spool=${SNAP_COMMON}/spool"\
