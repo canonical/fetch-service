e49a7beca9b438063450b26cb0057066592cac8e
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-06-06 16:51:22 -0300

    snap: add missing stage packages
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- snap/local/bin/fetch-service.sh
+++ snap/local/bin/fetch-service.sh
@@ -19,9 +19,9 @@ profile_port="$(snapctl get profile.port)"
 control_port="$(snapctl get control.port)"
 control_auth="$(snapctl get control.auth)"
 
-log_file="$(snapctl get log.file)"
+log_file="$(snapctl get log.file || true)"
 
-keyring="/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+keyring="S{SNAP}/usr/share/keyrings/ubuntu-archive-keyring.gpg"
 key="F6ECB3762474EDA9D21B7022871920D1991BC93C"
 export FETCH_APT_RELEASE_PUBLIC_KEY=$(gpg --export --armor --no-default-keyring --keyring "$keyring" "$key")
 export FETCH_SERVICE_AUTH="$control_auth"
