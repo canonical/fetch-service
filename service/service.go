1d4b6535ad05a85eebd5ad729e76531ef27b8e08
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-07-04 16:37:00 -0300

    service: move configuration files to subdirectory (#122)
    If we keep config files in the top level dir, the filesystem changes
    watcher is triggered by other changes such as log file updates.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- service/service.go
+++ service/service.go
@@ -268,8 +268,7 @@ func (svc *Service) Start() error {
 
 			case event, ok := <-svc.cfgw.Events:
 				if ok && event.Op&(fsnotify.Write|fsnotify.Create) != 0 {
-					//logger.Debugf("event: %v %s", event.Op, event.Name)
-					//logger.Infof("Configuration file changed: %s", event.Name)
+					logger.Infof("Configuration file changed: %s", event.Name)
 
 					switch filepath.Base(event.Name) {
 					case "acl.yaml":
