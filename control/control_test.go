3d49d20283738d01b08c372117caafb512fe8837
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-07-14 11:19:49 -0300

    control: fix error handling and add tests
    Properly handle error cases in control API calls, and improve test
    coverage.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- control/control_test.go
+++ control/control_test.go
@@ -20,30 +20,216 @@
 package control_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	. "gopkg.in/check.v1"
 
 	"github.com/canonical/fetch-service/control"
-	"github.com/canonical/fetch-service/logger"
-	"github.com/canonical/fetch-service/logger/testlogger"
 	"github.com/canonical/fetch-service/service/messages"
 )
 
 func Test(t *testing.T) { TestingT(t) }
 
 type controlSuite struct {
-	ch chan any
 }
 
 func (t *controlSuite) SetUpTest(c *C) {
-	testlogger.Init(logger.InfoLevel)
-	t.ch = make(chan any)
 }
 
 var _ = Suite(&controlSuite{})
+
+func (t *controlSuite) TestCreateSession(c *C) {
+	for _, tc := range []struct {
+		params  string // request body parameters
+		errmsg  string // session creation error
+		resCode int    // expected result code
+	}{
+		{`{"policy": "permissive"}`, "", 200},
+		{`{"policy": "permissive"}`, "oops", 400}, // session creation error
+		{"not json", "", 400},                     // bad parameters
+	} {
+		ch := make(chan any, 1)
+		server := control.NewServer(3333, ch, "foo:bar")
+		body := bytes.NewReader([]byte(tc.params))
+		req, err := http.NewRequest("POST", "http://localhost/session", body)
+		c.Assert(err, IsNil)
+		req.Header["Authorization"] = []string{"Basic Zm9vOmJhcg=="}
+
+		go func() {
+			msg := <-ch
+			cred := messages.SessionCredentials{Id: "A", Token: "B"}
+			if tc.errmsg != "" {
+				cred.Err = errors.New(tc.errmsg)
+			}
+			msg.(messages.CreateSession).Rch <- cred
+		}()
+
+		w := httptest.NewRecorder()
+		control.ServerCreateSession(server, w, req)
+		c.Check(w.Code, Equals, tc.resCode)
+
+		if tc.resCode == 200 {
+			var cred messages.SessionCredentials
+			err = json.Unmarshal(w.Body.Bytes(), &cred)
+			c.Assert(err, IsNil)
+			c.Check(cred, DeepEquals, messages.SessionCredentials{Id: "A", Token: "B"})
+		}
+	}
+}
+
+func (t *controlSuite) TestDeleteSessionToken(c *C) {
+	for _, tc := range []struct {
+		idvar   string // id encoded in the URL
+		body    string // request body
+		err     error  // end session error
+		resCode int    // expected result code
+	}{
+		{"012345", `{"token": "shalamacookie"}`, nil, 200},
+		{"", `{"token": "shalamacookie"}`, nil, 404},                                   // session ID not in parameters
+		{"012345", `{"token": "shalamacookie"}`, messages.ErrSessionNotFound, 404},     // session does not exist
+		{"012345", `{"token": "shalamacookie"}`, messages.ErrInvalidSessionToken, 400}, // token does not exist
+		{"012345", "not json", nil, 400},                                               // bad parameters
+		{"012345", `{"token": "shalamacookie"}`, errors.New("other error"), 500},       // something else
+	} {
+		ch := make(chan any, 1)
+		server := control.NewServer(3333, ch, "foo:bar")
+		body := bytes.NewReader([]byte(tc.body))
+		req, err := http.NewRequest("DELETE", "http://localhost/session/some-session-id/token", body)
+		c.Assert(err, IsNil)
+		if tc.idvar != "" {
+			req = mux.SetURLVars(req, map[string]string{"id": "012345"})
+		}
+		req.Header["Authorization"] = []string{"Basic Zm9vOmJhcg=="}
+
+		go func() {
+			msg := <-ch
+			msg.(messages.RevokeToken).Rch <- messages.RevokeTokenResult{
+				SessionId: tc.idvar,
+				Err:       tc.err,
+			}
+		}()
+
+		w := httptest.NewRecorder()
+		control.ServerDeleteSessionToken(server, w, req)
+		c.Check(w.Code, Equals, tc.resCode)
+
+		if tc.resCode == 200 {
+			var res messages.RevokeTokenResult
+			err = json.Unmarshal(w.Body.Bytes(), &res)
+			c.Assert(err, IsNil)
+			c.Check(res.Err, IsNil)
+			c.Check(res.SessionId, Equals, tc.idvar)
+		}
+	}
+}
+
+func (t *controlSuite) TestGetSessionReport(c *C) {
+	for _, tc := range []struct {
+		idvar   string // id encoded in the URL
+		err     error  // end session error
+		resCode int    // expected result code
+	}{
+		{"012345", nil, 200},
+		{"", nil, 404}, // session ID not in parameters
+		{"012345", messages.ErrSessionNotFound, 404}, // session does not exist
+		{"012345", messages.ErrSessionActive, 400},   // token does not exist
+		{"012345", errors.New("other error"), 500},   // something else
+	} {
+		ch := make(chan any, 1)
+		server := control.NewServer(3333, ch, "foo:bar")
+		req, err := http.NewRequest("GET", "http://localhost/session/some-session-id", nil)
+		c.Assert(err, IsNil)
+		if tc.idvar != "" {
+			req = mux.SetURLVars(req, map[string]string{"id": "012345"})
+		}
+		req.Header["Authorization"] = []string{"Basic Zm9vOmJhcg=="}
+
+		go func() {
+			msg := <-ch
+			msg.(messages.SessionReport).Rch <- messages.SessionReportResult{Err: tc.err}
+		}()
+
+		w := httptest.NewRecorder()
+		control.ServerGetSessionReport(server, w, req)
+		c.Check(w.Code, Equals, tc.resCode)
+
+		if tc.resCode == 200 {
+			var res messages.SessionReportResult
+			err = json.Unmarshal(w.Body.Bytes(), &res)
+			c.Assert(err, IsNil)
+			c.Check(res.Err, IsNil)
+		}
+	}
+}
+
+func (t *controlSuite) TestDeleteSession(c *C) {
+	for _, tc := range []struct {
+		idvar   string // id encoded in the URL
+		err     error  // end session error
+		resCode int    // expected result code
+	}{
+		{"012345", nil, 200},
+		{"", nil, 404}, // session ID not in parameters
+		{"012345", messages.ErrSessionNotFound, 404}, // session does not exist
+		{"012345", errors.New("other error"), 500},   // something else
+	} {
+		ch := make(chan any, 1)
+		server := control.NewServer(3333, ch, "foo:bar")
+		req, err := http.NewRequest("DELETE", "http://localhost/session/some-session-id", nil)
+		c.Assert(err, IsNil)
+		if tc.idvar != "" {
+			req = mux.SetURLVars(req, map[string]string{"id": "012345"})
+		}
+		req.Header["Authorization"] = []string{"Basic Zm9vOmJhcg=="}
+
+		go func() {
+			msg := <-ch
+			msg.(messages.EndSession).Rch <- tc.err
+		}()
+
+		w := httptest.NewRecorder()
+		control.ServerDeleteSession(server, w, req)
+		c.Check(w.Code, Equals, tc.resCode)
+	}
+}
+
+func (t *controlSuite) TestDeleteResources(c *C) {
+	for _, tc := range []struct {
+		idvar   string // id encoded in the URL
+		err     error  // end session error
+		resCode int    // expected result code
+	}{
+		{"012345", nil, 200},
+		{"", nil, 404}, // session ID not in parameters
+		{"012345", messages.ErrSessionNotFound, 404}, // session does not exist
+		{"012345", messages.ErrSessionActive, 400},   // session is still active
+		{"012345", errors.New("other error"), 500},   // something else
+	} {
+		ch := make(chan any, 1)
+		server := control.NewServer(3333, ch, "foo:bar")
+		req, err := http.NewRequest("DELETE", "http://localhost/resources/some-session-id", nil)
+		c.Assert(err, IsNil)
+		if tc.idvar != "" {
+			req = mux.SetURLVars(req, map[string]string{"id": "012345"})
+		}
+		req.Header["Authorization"] = []string{"Basic Zm9vOmJhcg=="}
+
+		go func() {
+			msg := <-ch
+			msg.(messages.DeleteResources).Rch <- tc.err
+		}()
+
+		w := httptest.NewRecorder()
+		control.ServerDeleteResources(server, w, req)
+		c.Check(w.Code, Equals, tc.resCode)
+	}
+}
 
 func (t *controlSuite) TestCheckAuth(c *C) {
 	for _, tc := range []struct {
@@ -57,7 +243,8 @@ func (t *controlSuite) TestCheckAuth(c *C) {
 		{"testuser:testpq", "Basic dGVzdHVzZXI6dGVzdHB3", false}, // bad passwd
 		{"testuser:testpw", "Basic dGVzdHVzZXI6dGVzdHB3", true},
 	} {
-		ctl := control.NewServer(1234, t.ch, tc.cred)
+		ch := make(chan any, 1)
+		ctl := control.NewServer(1234, ch, tc.cred)
 
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("GET", "http://some.url", nil)
@@ -69,10 +256,6 @@ func (t *controlSuite) TestCheckAuth(c *C) {
 }
 
 func (t *controlSuite) TestEndpointNoAuthentication(c *C) {
-	go func() {
-		msg := <-t.ch
-		msg.(messages.GetServiceStatus).Rch <- messages.ServiceStatus{}
-	}()
 
 	for _, tc := range []struct {
 		callEndpoint func(*control.Server, http.ResponseWriter, *http.Request)
@@ -80,7 +263,13 @@ func (t *controlSuite) TestEndpointNoAuthentication(c *C) {
 		{control.ServerGetServiceStatus},
 		{control.ServerDeleteSessionToken},
 	} {
-		ctl := control.NewServer(1234, t.ch, "testuser:testpw")
+		ch := make(chan any, 1)
+		ctl := control.NewServer(1234, ch, "testuser:testpw")
+
+		go func() {
+			msg := <-ch
+			msg.(messages.GetServiceStatus).Rch <- messages.ServiceStatus{}
+		}()
 
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("GET", "http://some.url", nil)
@@ -98,7 +287,8 @@ func (t *controlSuite) TestEndpointAuthenticationError(c *C) {
 		{control.ServerGetSessionReport},
 		{control.ServerDeleteResources},
 	} {
-		ctl := control.NewServer(1234, t.ch, "testuser:testpw")
+		ch := make(chan any, 1)
+		ctl := control.NewServer(1234, ch, "testuser:testpw")
 
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("GET", "http://some.url", nil)
