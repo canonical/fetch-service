1284e78317140ec37fea59afbf9a2ef4d0cd2afb
Dariusz Duda <dariusz.duda@canonical.com>
2024-09-12 14:39:32 -0400

    test: add checking if launchpad is accepted by sourcecraft
    Signed-off-by: Dariusz Duda <dariusz.duda@canonical.com>

--- inspectors/git/git_sourcecraft_test.go
+++ inspectors/git/git_sourcecraft_test.go
@@ -20,12 +20,18 @@
 package git_test
 
 import (
+	"io"
+	"net/http"
+	"strings"
+
 	. "gopkg.in/check.v1"
 
 	. "github.com/canonical/fetch-service/inspectors/common"
 	"github.com/canonical/fetch-service/inspectors/git"
 	"github.com/canonical/fetch-service/logger"
 	"github.com/canonical/fetch-service/logger/testlogger"
+	"github.com/canonical/fetch-service/metadata"
+	"github.com/canonical/fetch-service/metadata/opinions"
 )
 
 type sourcecraftGitSuite struct{}
@@ -41,4 +47,53 @@ func (s *sourcecraftGitSuite) TestSourcecraftGitInspectorInterface(c *C) {
 	ins := git.NewSourcecraftInspector()
 	c.Assert(ins, Implements, &iface)
 
+}
+
+func (s *sourcecraftGitSuite) TestUploadPackInspectorID(c *C) {
+	ins := git.NewSourcecraftInspector()
+	c.Assert(ins.ID(), Equals, "git.sourcecraft")
+
+}
+
+func (s *sourcecraftGitSuite) TestInspectSourcecraftGitRequest(c *C) {
+	for _, tc := range []struct {
+		url      string
+		approved bool
+	}{
+		// FIXME: using github as placeholder, final URLs will change
+		{"https://github.com:443/user/project.git/git-upload-pack", true},
+		{"https://git.launchpad.net:443/project/git-upload-pack", true},
+		{"https://git.launchpad.net:443/~user/project/+git/project/git-upload-pack", true},
+		{"https://github.com:443/user/project/git-upload-pack", true},
+		{"https://invalid.com:443/user/project.git/git-upload-pack", false},
+		{"http://github.com/user/project.git/git-upload-pack", false},
+		{"https://gothub.com:443/user/project.git/git-upload-pack", false},
+		{"ahttps://github.com:443/user/project.git/git-upload-pack", false},
+		{"https://github.com:443/user/project.git/git-upload-packs", false},
+		{"https://github.com:443/user/project.git/something-else", false},
+		{"https://git.launchpad.com:443/project/git-upload-pack", false},
+		{"https://git.lpad.net:443/~user/project/+git/project/git-upload-pack", false},
+	} {
+		ins := git.NewSourcecraftInspector()
+		a := metadata.NewArtefact()
+		a.CurrentDownload.URL = tc.url
+		a.CurrentDownload.RequestHeader = map[string][]string{
+			"Content-Type": {"application/x-git-upload-pack-request"},
+			"Accept":       {"application/x-git-upload-pack-result"},
+		}
+		a.RequestInspection["git.upload-pack"] = &Inspection{
+			Annotations: Annotation{"command": "fetch"},
+		}
+		a.Request, _ = http.NewRequest("GET", tc.url, nil)
+		a.Request.Body = io.NopCloser(strings.NewReader("0014command=ls-refs\n0000"))
+
+		err := ins.InspectRequest(a)
+		c.Assert(err, IsNil)
+
+		insp, ok := a.RequestInspection[ins.ID()]
+		c.Assert(ok, Equals, tc.approved)
+		if tc.approved {
+			c.Assert(insp.Opinion, Equals, opinions.Pending)
+		}
+	}
 }
