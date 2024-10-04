6122b9fcbf076b570105bf6fa69826759e231eda
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-10-04 13:50:28 -0300

    feat(inspectors): make sourcecraft url origin configurable (#245)
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ inspectors/craft/config/config.go
@@ -0,0 +1,73 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright 2024 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package config
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/canonical/fetch-service/glob"
+	"github.com/canonical/fetch-service/logger"
+	"github.com/canonical/fetch-service/utils"
+)
+
+var (
+	reSourcecraft = regexp.MustCompile(`^/(.+/)*([^/]+)/git-upload-pack$`)
+)
+
+func checkServerOrigin(cfg *CraftsInspectorConfig, u *url.URL) error {
+	origin := utils.NormalizedOrigin(u)
+
+	for _, h := range cfg.Origins {
+		if h.Match(origin) {
+			logger.Debugf("url origin matches %v\n", h)
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid origin %s", origin)
+}
+
+type CraftsInspectorConfig struct {
+	Origins []glob.Glob `yaml:"origins"` // List of allowed URL origin glob patterns
+}
+
+type SourcecraftUrlInfo struct {
+	Project string
+}
+
+func NewSourcecraftUrlInfo(u *url.URL, cfg *CraftsInspectorConfig) (*SourcecraftUrlInfo, error) {
+	if err := checkServerOrigin(cfg, u); err != nil {
+		return nil, err
+	}
+
+	m := reSourcecraft.FindStringSubmatch(u.Path)
+	if len(m) != 3 && len(m) != 2 {
+		return nil, errors.New("not a valid sourcecraft upload-pack path")
+	}
+	info := &SourcecraftUrlInfo{
+		Project: m[len(m)-1], // assuming the project name is encoded in the URL
+	}
+	info.Project, _ = strings.CutSuffix(info.Project, ".git")
+
+	return info, nil
+}
