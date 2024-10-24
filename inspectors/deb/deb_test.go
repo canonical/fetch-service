095e41a6336869752b08405f2e1456b9b436f712
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-10-24 06:48:24 -0300

    tests: additional tests to improve coverage (#278)
    * tests: add unit tests to improve test coverage
    * tests: fix/improve deb tests
    * tests: add git unpack test
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- inspectors/deb/deb_test.go
+++ inspectors/deb/deb_test.go
@@ -20,22 +20,20 @@
 package deb_test
 
 import (
-	"crypto/tls"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
+	"testing"
 
+	"github.com/gabriel-vasile/mimetype"
 	. "gopkg.in/check.v1"
 
+	"github.com/canonical/fetch-service/glob"
+	"github.com/canonical/fetch-service/inspectors/apt/config"
 	. "github.com/canonical/fetch-service/inspectors/common"
 	"github.com/canonical/fetch-service/inspectors/deb"
 	"github.com/canonical/fetch-service/inspectors/files"
 	"github.com/canonical/fetch-service/logger"
 	"github.com/canonical/fetch-service/logger/testlogger"
 	"github.com/canonical/fetch-service/metadata"
-	"github.com/canonical/fetch-service/metadata/digests"
+	"github.com/canonical/fetch-service/metadata/opinions"
 )
 
 type debSuite struct{}
@@ -46,61 +44,89 @@ func (t *debSuite) SetUpTest(c *C) {
 
 var _ = Suite(&debSuite{})
 
-const (
-	debURL = "http://launchpadlibrarian.net/592566337/hello_2.10-2ubuntu4_amd64.deb"
-)
+func Test(t *testing.T) { TestingT(t) }
 
-func (s *debSuite) TestDebInspector(c *C) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+func getTestAptConfig() config.AptInspectorConfig {
+	return config.AptInspectorConfig{
+		Repositories: map[string]config.AptInspectorConfigRepository{
+			"default": {
+				Urls:       []glob.Glob{glob.MustCompile("http://archive.ubuntu.com/ubuntu")},
+				Dists:      []glob.Glob{glob.MustCompile("jammy")},
+				Components: []glob.Glob{glob.MustCompile("main")},
+				PublicKey:  "",
+			},
+		},
 	}
-	client := &http.Client{
-		Transport: transport,
-		Timeout:   30 * time.Second,
-	}
+}
 
-	req, err := http.NewRequest("GET", debURL, nil)
-	c.Assert(err, IsNil)
+func (s *debSuite) TestDebInspectorID(c *C) {
+	ins := deb.NewDebInspector(getTestAptConfig())
+	c.Assert(ins.ID(), Equals, "deb")
+}
 
-	resp, err := client.Do(req)
-	c.Assert(err, IsNil)
-	c.Assert(resp.StatusCode, Equals, 200)
-
-	defer resp.Body.Close()
-
-	tmp := c.MkDir()
-	dest, err := os.Create(filepath.Join(tmp, "290d07339dde2735121ab03e525ca6593c395a42.bin"))
-	c.Assert(err, IsNil)
-
-	_, err = io.Copy(dest, resp.Body)
-	c.Assert(err, IsNil)
-
-	dest.Close()
-
-	h, _ := digests.NewSha1Digest("290d07339dde2735121ab03e525ca6593c395a42")
-	a := metadata.NewArtefact()
-	a.Metadata.Type = "application/vnd.debian.binary-package"
-	a.Metadata.Sha1 = h
-
+func (s *debSuite) TestDebInspectorInterface(c *C) {
 	var iface Inspector
-	ins := deb.DebInspector{}
+	ins := deb.NewDebInspector(config.AptInspectorConfig{})
 	c.Assert(ins, Implements, &iface)
 
-	// TODO: inject Packages.xz data into inspection context to validate the deb file
+}
 
-	f, err := files.OpenArtefactFile(filepath.Join(tmp, "290d07339dde2735121ab03e525ca6593c395a42.bin"))
-	c.Assert(err, IsNil)
-	defer f.Close()
+func (s *debSuite) TestInspectRequest(c *C) {
+	for _, tc := range []struct {
+		url     string
+		pending bool
+	}{
+		{"http://archive.ubuntu.com/ubuntu/pool/main/libe/liberror-perl/liberror-perl_0.17029-1_all.deb", true},
+		{"http://archive.ubuntu.com/ubuntu/pool/main/b/borgmatic/borgmatic_1.7.9-0ubuntu1~bpo22.04.1_all.deb", true},
+		{"http://archive.ubuntu.com/ubuntu/pool/universe/b/borgmatic/borgmatic_1.7.9-0ubuntu1~bpo22.04.1_all.deb", true},
+		{"http://not-archive.ubuntu.com/ubuntu/pool/main/libe/liberror-perl/liberror-perl_0.17029-1_all.deb", false},
+	} {
+		ins := deb.NewDebInspector(getTestAptConfig())
+		a := metadata.NewArtefact()
+		a.CurrentDownload = metadata.Download{URL: tc.url}
 
-	err = ins.InspectArtefact(f, a)
-	c.Assert(err, IsNil)
+		err := ins.InspectRequest(a)
+		c.Assert(err, IsNil)
 
-	c.Check(a.Metadata.Name, Equals, "hello")
-	c.Check(a.Metadata.Version, Equals, "2.10-2ubuntu4")
-	c.Check(a.Metadata.Vendor, Equals, "Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>")
-	c.Check(a.Metadata.Description, Equals, "Example package based on GNU hello")
-	c.Check(a.Metadata.Author, Equals, "") // FIXME: deb inspector needs a better author email parser
-	c.Check(a.Metadata.AuthorEmail, Equals, "Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>")
-	c.Check(a.Metadata.License, Equals, "") // FIXME: copyright file is not in machine-readable format
-	//c.Check(a.Metadata.Annotations["deb.debian-binary.version"].Value, Equals, "2.0")
+		insp, ok := a.RequestInspection[ins.ID()]
+		c.Assert(ok, Equals, tc.pending, Commentf("test case: %+v", tc))
+		if tc.pending {
+			c.Assert(insp.Opinion, Equals, opinions.Pending)
+		}
+	}
+}
+
+func (s *debSuite) TestDebArtefactInspector(c *C) {
+	for _, tc := range []struct {
+		testfile string
+		approved bool
+	}{
+		{"testdata/hello_2.10-2ubuntu4_amd64.deb", true},
+		{"testdata/2048.package", false},
+	} {
+		a := metadata.NewArtefact()
+		a.Metadata.Type = "application/vnd.debian.binary-package"
+		a.MimeType = mimetype.Lookup("application/vnd.debian.binary-package")
+
+		f, err := files.OpenArtefactFile(tc.testfile)
+		c.Assert(err, IsNil)
+		defer f.Close()
+
+		ins := deb.NewDebInspector(getTestAptConfig())
+		a.SetRequestPending(ins, "test")
+		err = ins.InspectArtefact(f, a)
+		c.Assert(err, IsNil)
+		c.Assert(a.Approved(), Equals, tc.approved)
+
+		if tc.approved {
+			c.Check(a.Metadata.Name, Equals, "hello")
+			c.Check(a.Metadata.Version, Equals, "2.10-2ubuntu4")
+			c.Check(a.Metadata.Vendor, Equals, "Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>")
+			c.Check(a.Metadata.Description, Equals, "Example package based on GNU hello")
+			c.Check(a.Metadata.Author, Equals, "") // FIXME: deb inspector needs a better author email parser
+			c.Check(a.Metadata.AuthorEmail, Equals, "Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>")
+			c.Check(a.Metadata.License, Equals, "GFDL-1.3-or-later and/or GPL-3.0-or-later") // this is what licensecheck says
+			c.Check(a.Metadata.Architecture, Equals, "amd64")
+		}
+	}
 }
---
+++ inspectors/git/utils_test.go
@@ -0,0 +1,84 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright 2024 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package git_test
+
+import (
+	"os"
+	"path/filepath"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/canonical/fetch-service/inspectors/git"
+)
+
+type utilsSuite struct{}
+
+var _ = Suite(&utilsSuite{})
+
+func (s *utilsSuite) TestUnpackObjects(c *C) {
+	for _, tc := range []struct {
+		testfile string
+		errorMsg string
+	}{
+		{"testdata/sourcepkg.raw", ""},
+		{"testdata/bad-data.raw", ".* invalid syntax"},
+	} {
+		dir := c.MkDir()
+		f, err := os.Open(tc.testfile)
+		c.Assert(err, IsNil)
+		defer f.Close()
+
+		err = git.UnpackObjects(f, dir)
+		if tc.errorMsg == "" {
+			c.Assert(err, IsNil)
+			_, err = os.Stat(filepath.Join(dir, ".git", "objects"))
+			c.Assert(err, IsNil)
+		} else {
+			c.Assert(err, ErrorMatches, tc.errorMsg)
+		}
+
+	}
+}
+
+func (s *utilsSuite) TestCheckout(c *C) {
+	for _, tc := range []struct {
+		wants    string
+		errorMsg string
+	}{
+		{"10fce2c8e3a341998ffd2aa4e27b02699d1bb5ad", ""},
+		{"not-a-valid-ref", "exit status 1"},
+	} {
+		dir := c.MkDir()
+		f, err := os.Open("testdata/sourcepkg.raw")
+		c.Assert(err, IsNil)
+		defer f.Close()
+
+		err = git.UnpackObjects(f, dir)
+		c.Assert(err, IsNil)
+		err = git.Checkout(dir, tc.wants)
+		if tc.errorMsg == "" {
+			c.Assert(err, IsNil)
+			_, err = os.Stat(filepath.Join(dir, "sourcecraft.yaml"))
+			c.Assert(err, IsNil)
+		} else {
+			c.Assert(err, ErrorMatches, tc.errorMsg)
+		}
+	}
+}
