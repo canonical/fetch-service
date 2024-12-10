66271b6ee76e61085c0ab5e86cf19e48ee7af469
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-12-10 17:13:36 -0300

    tests: improve coverage for proxy certificate helpers (#309)
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- proxy/cert_test.go
+++ proxy/cert_test.go
@@ -59,10 +59,66 @@ func (t *certSuite) TestLoadCertificate(c *C) {
 	err = os.WriteFile(keyPath, []byte("key data"), 0644)
 	c.Assert(err, IsNil)
 
-	certData, keyData, err := proxy.LoadCertificate(certPath, keyPath)
-	c.Assert(err, IsNil)
-	c.Check(string(certData), Equals, "cert data")
-	c.Check(string(keyData), Equals, "key data")
+	for _, tc := range []struct {
+		cert   string
+		key    string
+		errMsg string
+	}{
+		{certPath, keyPath, ""},
+		{certPath, "", "HTTPS proxy key path not specified"},
+		{"", "", "HTTPS proxy certificate path not specified"},
+		{"/other/path", keyPath, "open /other/path: no such file or directory"},
+		{certPath, "/other/path", "open /other/path: no such file or directory"},
+	} {
+		certData, keyData, err := proxy.LoadCertificate(tc.cert, tc.key)
+		if tc.errMsg == "" {
+			c.Assert(err, IsNil)
+			c.Check(string(certData), Equals, "cert data")
+			c.Check(string(keyData), Equals, "key data")
+		} else {
+			c.Assert(err, ErrorMatches, tc.errMsg)
+		}
+	}
+}
+
+func (t *certSuite) TestUpdateCert(c *C) {
+	for _, tc := range []struct {
+		dryRun  bool
+		payload []byte
+		errMsg  string
+	}{
+		{true, append(append(testutils.ProxyCert, []byte("\n\n")...), testutils.ProxyKey...), ""},
+		{false, append(append(testutils.ProxyCert, []byte("\n\n")...), testutils.ProxyKey...), ""},
+		{true, []byte{}, "cannot parse certificate and key"},
+		{true, testutils.ProxyCert, "cannot parse certificate and key"},
+		{true, testutils.ProxyKey, "cannot parse certificate and key"},
+	} {
+		dir := c.MkDir()
+		certPath := filepath.Join(dir, "cert.txt")
+		keyPath := filepath.Join(dir, "key.txt")
+		err := proxy.UpdateCert(tc.dryRun, tc.payload, certPath, keyPath)
+
+		if tc.errMsg == "" {
+			c.Assert(err, IsNil)
+			if tc.dryRun {
+				_, err := os.Stat(certPath)
+				c.Assert(err != nil && os.IsNotExist(err), Equals, true)
+
+				_, err = os.ReadFile(keyPath)
+				c.Assert(err != nil && os.IsNotExist(err), Equals, true)
+			} else {
+				cert, err := os.ReadFile(certPath)
+				c.Assert(err, IsNil)
+				c.Check(slices.Equal(cert, testutils.ProxyCert), Equals, true)
+
+				key, err := os.ReadFile(keyPath)
+				c.Assert(err, IsNil)
+				c.Check(slices.Equal(key, testutils.ProxyKey), Equals, true)
+			}
+		} else {
+			c.Assert(err, ErrorMatches, tc.errMsg)
+		}
+	}
 }
 
 func (t *certSuite) TestUpdateCertFiles(c *C) {
