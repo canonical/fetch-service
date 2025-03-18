bb851fd70d9740f224c5e22eb6e19d53902e2bb8
Rafid Bin Mostofa <rafid.mostofa@canonical.com>
2025-03-18 18:30:52 +0600

    fix(apt.release): make InRelease description field optional (#334)
    The rationale behind this change is adding support for ESM archives.
    Quite unfortunately, many (if not all) of them have no ``Description``
    field, which is quite fine since it's an optional field anyway per the
    Debian wiki. [1]
    
    [1] https://wiki.debian.org/DebianRepository/Format#A.22Release.22_files

--- inspectors/apt/release.go
+++ inspectors/apt/release.go
@@ -262,7 +262,6 @@ func (ins *AptReleaseInspector) InspectArtifact(f ArtifactReader, a ResponseArti
 		"Date",
 		"Architectures",
 		"Components",
-		"Description",
 	}
 
 	for _, k := range expected_fields {
@@ -312,11 +311,15 @@ func (ins *AptReleaseInspector) InspectArtifact(f ArtifactReader, a ResponseArti
 
 	repo := strings.TrimSuffix(a.DownloadURL(), "/InRelease")
 
+	desc := fields["Description"]
+	if desc == "" {
+		desc = fmt.Sprintf("%s %s", fields["Origin"], fields["Suite"])
+	}
 	a.SetArtifactMetadata(ArtifactMetadata{
 		Type:        mimetypes.AptRelease,
 		Name:        "InRelease",
 		Version:     fields["Codename"],
-		Description: fields["Description"],
+		Description: desc,
 		Vendor:      fields["Origin"],
 		Author:      fields["Origin"],
 	})
