ae84bbf0c010316d2eb17ecec0d5c4b398def5ed
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2025-09-12 08:48:59 -0300

    chore: fix debug message
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- inspectors/apt/packages.go
+++ inspectors/apt/packages.go
@@ -442,7 +442,7 @@ func (ins *AptPackagesInspector) getOriginAlias(origin, cfgName string, slog log
 
 	alias := repos.BaseUrlAlias
 	if alias == "" {
-		slog.Debug("origin not aliased: %s", origin)
+		slog.Debugf("origin not aliased: %s", origin)
 		return origin
 	}
 
